bffa6f6e2b8932283e318632a2132a52c526d5bf
kobai <vkobayashi106@gmail.com>
2019-04-06 22:28:34 -0400

    ¯\_(ツ)_/¯

--- image_downloader.py
+++ image_downloader.py
@@ -4,8 +4,16 @@ import os
 
 #%%
 os.mkdir('./imgs')
+os.mkdir('./eval')
+os.mkdir('./predict')
+os.mkdir('./predict/imgs')
 
 #%%
 for i in range(649):
   num = '%03d' % (i+1)
   urllib.request.urlretrieve('https://assets.pokemon.com/assets/cms2/img/pokedex/full/%s.png' % num, './imgs/%s.png' % str(i+1))
+
+#%%
+for i in range(650,722):
+  num = '%03d' % i
+  urllib.request.urlretrieve('https://assets.pokemon.com/assets/cms2/img/pokedex/full/%s.png' % num, './eval/%s.png' % str(i))
--- preprocess.py
+++ preprocess.py
@@ -9,15 +9,26 @@ import pandas as pd
 
 #%%
 df = pd.read_csv('pokemon_stat.csv')
-type_list = df.head(649)['type_1'].to_list()
-all_types = list(set(type_list))
+train_list = df.head(649)['type_1'].to_list()
+eval_list = df.head(721).tail(721-649)['type_1'].to_list()
+all_types = list(set(train_list))
 
 #%%
 for ptype in all_types:
   os.mkdir('./imgs/%s' % ptype)
 
 #%%
+for ptype in all_types:
+  os.mkdir('./eval/%s' % ptype)
+
+#%%
 for img in os.listdir('./imgs'):
   if re.match('\d+.png', img):
     index = int(img.split('.')[0])
-    os.rename('./imgs/%s' % img, './imgs/%s/%s' % (type_list[index-1], img))
+    os.rename('./imgs/%s' % img, './imgs/%s/%s' % (train_list[index-1], img))
+
+#%%
+for img in os.listdir('./eval'):
+  if re.match('\d+.png', img):
+    index = int(img.split('.')[0])
+    os.rename('./eval/%s' % img, './eval/%s/%s' % (eval_list[index-1-649], img))
